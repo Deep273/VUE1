bf475d9bc238b23a9d73cb76e5e6925f0c2b2abe
Deep273 <soul42460@gmail.com>
2025-02-19 01:02:12 +0700

    practical work7

--- js/main.js
+++ js/main.js
@@ -3,35 +3,61 @@ let app = new Vue({
 
     data: {
         product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
+        brand: 'Vue Mastery',
         altText: "A pair of socks",
-        inStock: false,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
+                onSale: true
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
             }
         ],
         cart: 0,
+        selectedVariant: 0,
     },
 
     methods: {
         addToCart() {
             this.cart += 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         },
         deleteToCart(){
             this.cart -= 1
         },
 
-    }
+
+    },
+
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.variants[this.selectedVariant].onSale) {
+                return this.brand + ' ' + this.product + 'is on sale!';
+            } else {
+                return this.brand + ' ' + this.product +  'is not on sale';
+            }
+        }
+
+    },
+
 })
